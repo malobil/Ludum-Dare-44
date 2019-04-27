bc63c236d3fd8228a71e57517f2705dedaf5df5a
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-04-27 11:57:56 +0200

    script player

--- Assets/Scripts/Script_Player.cs
+++ Assets/Scripts/Script_Player.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Script_Player : MonoBehaviour
 {
+    public static Script_Player Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
