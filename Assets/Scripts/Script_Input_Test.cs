87ec35ba2c98d47ebe78855268c6079d159583ef
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2019-04-27 14:26:03 +0200

    wip input manager

--- Assets/Scripts/Script_Input_Test.cs
+++ Assets/Scripts/Script_Input_Test.cs
@@ -7,44 +7,91 @@ public class Script_Input_Test : MonoBehaviour
     [Range(1,4)]
     [SerializeField] private int i_player_number = 1;
 
+    private string s_controller_type = "" ;
     private string s_player_number = "";
 
     private void Start()
-    {
-        s_player_number = i_player_number.ToString("");
+    {        
+        if (Input.GetJoystickNames().Length > i_player_number - 1)
+        {
+            s_controller_type = Input.GetJoystickNames()[i_player_number -1];
+            s_player_number = i_player_number.ToString("");
+            Debug.Log(s_controller_type);
+           
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetButtonDown("Square_P" + s_player_number))
+        if(s_controller_type != "")
         {
-            Debug.Log("Square_P" + s_player_number);
-        }
+            if (s_controller_type == "Wireless Controller")
+            {
+                if (Input.GetButtonDown("Square_P" + s_player_number))
+                {
+                    Debug.Log("Square_P" + s_player_number);
+                }
 
-        if (Input.GetButtonDown("R1_P" + s_player_number))
-        {
-            Debug.Log("R1_P" + s_player_number);
-        }
+                if (Input.GetButtonDown("R1_P" + s_player_number))
+                {
+                    Debug.Log("R1_P" + s_player_number);
+                }
 
-        if(Input.GetAxis("R2_P" + s_player_number) > 0)
-        {
-            Debug.Log("R2_P" + s_player_number);
-        }
+                if (Input.GetAxis("R2_P" + s_player_number) > 0)
+                {
+                    Debug.Log("R2_P" + s_player_number);
+                }
 
-        if (Input.GetAxis("L2_P" + s_player_number) > 0)
-        {
-            Debug.Log("L2_P" + s_player_number);
-        }
+                if (Input.GetAxis("L2_P" + s_player_number) > 0)
+                {
+                    Debug.Log("L2_P" + s_player_number);
+                }
 
-        if (Input.GetAxis("Horizontal_P" + s_player_number) > 0)
-        {
-            Debug.Log("Horizontal_P" + s_player_number);
-        }
+                if (Input.GetAxis("Horizontal_P" + s_player_number) > 0)
+                {
+                    Debug.Log("Horizontal_P" + s_player_number);
+                }
 
-        if (Input.GetAxis("Vertical_P" + s_player_number) > 0)
-        {
-            Debug.Log("Vertical_P" + s_player_number);
+                if (Input.GetAxis("Vertical_P" + s_player_number) > 0)
+                {
+                    Debug.Log("Vertical_P" + s_player_number);
+                }
+            }
+            else
+            {
+                if (Input.GetAxis("RT_P" + s_player_number) > 0)
+                {
+                    Debug.Log("RT_P" + s_player_number);
+                }
+
+                if (Input.GetAxis("LT_P" + s_player_number) > 0)
+                {
+                    Debug.Log("RT_P" + s_player_number);
+                }
+
+                if (Input.GetAxis("RB_P" + s_player_number) > 0)
+                {
+                    Debug.Log("RB_P" + s_player_number);
+                }
+
+                if (Input.GetAxis("X_P" + s_player_number) > 0)
+                {
+                    Debug.Log("X_P" + s_player_number);
+                }
+
+                if (Input.GetAxis("Horizontal_P" + s_player_number) > 0)
+                {
+                    Debug.Log("Horizontal_P" + s_player_number);
+                }
+
+                if (Input.GetAxis("Vertical_P" + s_player_number) > 0)
+                {
+                    Debug.Log("Vertical_P" + s_player_number);
+                }
+            }
+
         }
     }
+           
 }
